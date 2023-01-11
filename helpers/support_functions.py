ca7177c91eb1cc4910e41c40b1a79eaeb4e9edf1
Paweł Mróz <pawelmroz@gmail.com>
2023-01-11 18:05:35 +0100

    Adding helpers folder with support functions

---
+++ helpers/support_functions.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+from helpers.DataGenerator import DataGenerator
+
+
+
+def hover_over_element(driver_instance, xpath):
+    elem = driver_instance.find_element(By.XPATH, xpath)
+    hover = ActionChains(driver_instance).move_to_element(elem)
+    hover.perform()
+
+def wait_for_visibility_of_element(driver_instance, xpath, time_to_wait=10):
+    try:
+        elem = WebDriverWait(driver_instance, time_to_wait).until(EC.visibility_of_element_located((By.XPATH, xpath)))
+    except TimeoutException:
+        elem = False
+    return False
+
+def wait_for_invisibility_of_element(inv_driver_instance, xppath, time_to_wait=8):
+    inv_element = WebDriverWait(inv_driver_instance,time_to_wait).until(EC.invisibility_of_element_located((By.XPATH.xpath)))
+    return inv_element
+
+# test = DataGenerator.generateProperEmail()
+#
+# print(test)
+
