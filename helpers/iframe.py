adc31fb7e85683de15512e5930403ed517b703ab
Paweł Mróz <pawelmroz@gmail.com>
2023-01-11 22:07:57 +0100

    Adding another supporting function

--- helpers/iframe.py
+++ helpers/iframe.py
@@ -1,5 +1,8 @@
 from helpers.support_functions import *
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
 
 iframe_tab = 'iframe-header'
 iframe_content = 'iframe-content'
@@ -8,13 +11,16 @@ button1 = 'simpleButton1'
 button2 = 'simpleButton2'
 message = 'whichButtonIsClickedMessage'
 
+
 def click_iframe_tab(driver_instance):
     elem = driver_instance.find_element(By.ID, iframe_tab)
     elem.click()
 
+
 def iframe_content_visible(driver_instance):
-    elem = wait_for_visibility_of_element(driver_instance, iframe_content)
+    elem = wait_for_visibility_of_element_by_id(driver_instance, iframe_content)
     return elem.is_displayed()
+
 
 def click_inside_iframe(driver_instance):
     wait_for_visibility_of_element(driver_instance, iframe_main, time_to_wait=5)
@@ -25,6 +31,7 @@ def click_inside_iframe(driver_instance):
         return True
     else:
         return False
+
 
 def click_inside_iframe_two(driver_instance):
     wait_for_visibility_of_element(driver_instance, iframe_main, time_to_wait=1)
--- helpers/support_functions.py
+++ helpers/support_functions.py
@@ -18,10 +18,17 @@ def wait_for_visibility_of_element(driver_instance, xpath, time_to_wait=10):
         elem = WebDriverWait(driver_instance, time_to_wait).until(EC.visibility_of_element_located((By.XPATH, xpath)))
     except TimeoutException:
         elem = False
-    return False
+    return elem
 
-def wait_for_invisibility_of_element(inv_driver_instance, xppath, time_to_wait=8):
-    inv_element = WebDriverWait(inv_driver_instance,time_to_wait).until(EC.invisibility_of_element_located((By.XPATH.xpath)))
+def wait_for_visibility_of_element_by_id(driver_instance, id, time_to_wait=10):
+    try:
+        elem = WebDriverWait(driver_instance, time_to_wait).until(EC.visibility_of_element_located((By.ID, id)))
+    except TimeoutException:
+        elem = False
+    return elem
+
+def wait_for_invisibility_of_element(inv_driver_instance, xpath, time_to_wait=8):
+    inv_element = WebDriverWait(inv_driver_instance, time_to_wait).until(EC.invisibility_of_element_located((By.XPATH, xpath)))
     return inv_element
 
 # test = DataGenerator.generateProperEmail()
